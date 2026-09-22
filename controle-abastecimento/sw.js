19ef521b4b9af11999e76a12cf5533f53b69b306
checklist-vtr <checklist.viaturas.oficial@gmail.com>
2026-09-22 13:36:49 -0300

    Otimiza desempenho e interface mobile
    Otimiza chamadas ao banco de dados, inicialização do Controle de Abastecimento e reorganiza o cabeçalho do Painel Administrativo em dispositivos móveis.

--- controle-abastecimento/sw.js
+++ controle-abastecimento/sw.js
@@ -1,4 +1,4 @@
-const CACHE="sigvtr-abastecimento-v2";
+const CACHE="sigvtr-abastecimento-v3";
 const STATIC=["./","./index.html","./app.js","./manifest.json"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
